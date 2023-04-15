62bb47f3fe721bba484a7184ec74e9b0d2836cf1
Sofiane Boumedine <sboumedine@LM-00202.local>
2023-04-16 01:32:53 +0200

    Message Board Tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -71,9 +71,9 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'yarn dev',
-    url: 'http://127.0.0.1:3000',
-    reuseExistingServer: true,
+    command: "yarn dev",
+    port: 1337, // should match the port you gave in our vite.config file
+    reuseExistingServer: !process.env.CI,
   },
   // fullyParallel: false,
   // projects: [
